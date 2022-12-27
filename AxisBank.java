c4c8533e9fb88a2468531ba9b74504b475d0d749
saranyamanikandan2022 <119424959+saranyamanikandan2022@users.noreply.github.com>
2022-12-27 10:16:01 +0530

    Update AxisBank.java

--- AxisBank.java
+++ AxisBank.java
@@ -1,5 +1,16 @@
 package Org.system;
+ public class AxisBank extends BankInfo{
+	public void deposit(){
+	System.out.println("deposit");
+	}
+public static void main(String[] args) {
+	AxisBank bank = new AxisBank();
+    bank.saving();
+    bank.fixed();
+    bank.deposit();
+    System.out.println(AxisBank.equal(deposit));
+}
 
-public class AxisBank {
+	}
 
 }
